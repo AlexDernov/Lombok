d8c81ef10f9627323097c909fc5c137ccf0ea1ed
Alexandra Dernova <alexder@gmx.net>
2023-12-15 13:14:08 +0100

    Add getGradeOfCourse

--- src/main/java/UniversityService.java
+++ src/main/java/UniversityService.java
@@ -1,12 +1,19 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class UniversityService {
     Map<String, Course> alleCourseMap = new HashMap<>();
-    University ourUniversity = University.builder()
-            .courseMap(alleCourseMap)
-            .build();
-public double gradeOfCourse(String nameOfCourse) {
+    University ourUniversity;
+public double getGradeOfCourse(String idOfCourse) {
+    Course course = ourUniversity.getCourseMap().get(idOfCourse);
+    List<Student> list = course.getStudentsList();
+    double gradeTotal = 0;
+    for (Student student : list) {
+        gradeTotal += student.getGrade();
+    }return gradeTotal/list.size();
 
 }
 public double gradeOfUniversity(String nameOfUniversity){
